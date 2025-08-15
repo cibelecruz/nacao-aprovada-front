d2017e7d43e57e0fd0cd9fb26304d45bf00f4b0e
Matteus Varlesse <varlesse04@gmail.com>
2025-08-15 17:36:26 -0300

    fix: add withCredentials in axios

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -4,6 +4,7 @@ import { auth } from './firebase'
 
 export const api = Axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL,
+  withCredentials: true,
 })
 
 async function getTokenFromSession() {
